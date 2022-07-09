117ee653e861b356cdb0c6cca6615de8e1ce17f3
FMatos <FMatos@DESKTOP-R31F89O>
2022-07-09 00:21:18 -0400

    Add project files.

---
+++ SeleniumProject/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumProject
+{
+    class Program
+    {
+        public void login(string username, string password)
+        {            
+            IWebDriver ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Cookies.DeleteAllCookies();
+            ChromeDriver.Navigate().GoToUrl("http://the-internet.herokuapp.com/login");
+            ChromeDriver.FindElement(By.Name("username")).SendKeys(username);
+            ChromeDriver.FindElement(By.Name("password")).SendKeys(password);
+            Thread.Sleep(3000);
+            ChromeDriver.FindElement(By.ClassName("radius")).Click();
+
+            Console.WriteLine($"TESTING RESULT: {ChromeDriver.FindElement(By.ClassName("error")).Text}");
+            Thread.Sleep(3000);
+            ChromeDriver.Quit();
+        }
+
+        static void Main(string[] args)
+        {            
+            Program program = new Program();
+            program.login("tomsmith", "WrongPassword");
+            program.login("WrongUser", "SuperSecretPassword");
+        }
+    }
+}
+
+
+
